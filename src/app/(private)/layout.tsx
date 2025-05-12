765b0728b0be98767e5c83bd871de90cea174e52
EstherrWolf <kiet.vo@stunited.vn>
2025-05-12 11:50:44 +0700

    fix build

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,21 +1,26 @@
 "use client"
 
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 interface PrivateRouteProps {
   children: React.ReactNode;
 }
 
-const isAuthenticated = (): boolean => {
-  if (typeof window !== 'undefined') {
-    return !!localStorage.getItem("theme");
-  }
-  return false
-};
-
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
-  if (!isAuthenticated()) {
-    return <Link href={'/login'} replace />;
+  const [authenticated, setAuthenticated] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const auth = !!localStorage.getItem("theme"); // hoặc token nếu bạn đang kiểm tra đăng nhập
+    setAuthenticated(auth);
+  }, []);
+
+  if (authenticated === null) {
+    return null; // hoặc spinner, loading...
+  }
+
+  if (!authenticated) {
+    return <Link href="/login" replace />;
   }
 
   return <>{children}</>;
--- src/shared/store/index.ts
+++ src/shared/store/index.ts
@@ -1,19 +1,16 @@
-export const getStorageData = (data: string) => {
-  if (typeof window !== "undefined") {
-    return JSON.parse(localStorage.getItem(data) || "{}");
-  }
-}
+export const getStorageData = (key: string) => {
+  if (typeof window === "undefined") return null;
+  return localStorage.getItem(key);
+};
 
 export const toStoredData = (data: unknown) => JSON.stringify(data);
 
-export const setStorageData = (key: string, data: unknown): void => {
-  if (typeof window !== "undefined") {
-    localStorage.setItem(key, toStoredData(data));
-  }
+export const setStorageData = (key: string, data: unknown) => {
+  if (typeof window === "undefined") return null;
+  return localStorage.setItem(key, toStoredData(data));
 };
 
-export const removeStorageData = (key: string): void => {
-  if (typeof window !== "undefined") {
-    localStorage.removeItem(key);
-  }
+export const removeStorageData = (key: string) => {
+  if (typeof window === "undefined") return null;
+  return localStorage.removeItem(key);
 };
